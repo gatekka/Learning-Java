de646da96ae46cb86c84a166db8bd01de765320f
chris <94351434+gatekka@users.noreply.github.com>
2024-03-19 17:16:14 -0700

    Made pyramid prettier if user inputs value >= 10

--- Numerical Triangle/Numerical_Triangle.java
+++ Numerical Triangle/Numerical_Triangle.java
@@ -12,10 +12,19 @@ public class Numerical_Triangle
   System.out.print("Enter a number: ");
   int num = in.nextInt();
 
+  String pyramid = "";
   for(int i = 1; i <= num; i++)
   {
-   String pyramid = i + " ";
-   System.out.println(pyramid.repeat(i));
+   if(num >= 10 && i < 10)
+   {
+    pyramid = "0" + i + " ";
+    System.out.println(pyramid.repeat(i));
+   }
+   else
+   {
+    pyramid = i + " ";
+    System.out.println(pyramid.repeat(i));
+   }
   }  
  }
 }
