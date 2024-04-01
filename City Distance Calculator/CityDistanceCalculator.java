0749d3367632214c6785f39789e13b0053de960c
chris <94351434+gatekka@users.noreply.github.com>
2024-03-31 19:09:46 -0700

    Added files

---
+++ City Distance Calculator/CityDistanceCalculator.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CityDistanceCalculator
+{
+    public static void main(String[] args)
+    {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the city to travel from: ");
+        String from = scanner.nextLine();
+        System.out.print("Enter the city to travel to: ");
+        String to = scanner.nextLine();
+        scanner.close();
+
+        ArrayList<String> fromCity = new ArrayList<String>();
+        fromCity.add("Los Angeles");
+        fromCity.add("San Francisco");
+        fromCity.add("Portland");
+        ArrayList<String> toCity = new ArrayList<String>();
+        toCity.add("Seattle");
+        toCity.add("Denver");
+        toCity.add("Chicago");
+
+        int[][] distance = {
+            {1135, 1016, 2055},
+            {807,1250,2128},
+            {174,1240,2121}
+        };
+
+        System.out.println(distance[fromCity.indexOf(from)][toCity.indexOf(to)]);
+
+    }
+}
